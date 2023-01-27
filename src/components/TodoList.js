bf69bc741247766aa63ce483ffa5d8816377d621
yeabtsega45 <yabtsega2022@gmail.com>
2023-01-27 12:34:21 +0300

    creating add task function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,6 +4,21 @@ function TodoList() {
   // Tasks (ToDo List) State
   const [toDo, setToDo] = useState([]);
 
+  // Temp State
+  const [newTask, setNewTask] = useState("");
+  const [updateData, setUpdateData] = useState("");
+
+  // Add task
+  ///////////////////////////
+  const addTask = () => {
+    if (newTask) {
+      let num = toDo.length + 1;
+      let newEntry = { id: num, title: newTask, status: false };
+      setToDo([...toDo, newEntry]);
+      setNewTask("");
+    }
+  };
+
   return <div></div>;
 }
 
