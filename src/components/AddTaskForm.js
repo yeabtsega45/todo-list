d6fc8dec29731913e46a4ae025745ada2b3ff143
yeabtsega45 <yabtsega2022@gmail.com>
2023-01-27 13:23:23 +0300

    adding input with on change event listner

--- src/components/AddTaskForm.js
+++ src/components/AddTaskForm.js
@@ -1,2 +1,19 @@
 const AddTaskForm = ({ newTask, setNewTask, addTask }) => {
-    return(
+  return (
+    <>
+      {/* Add Task */}
+      <div className="row">
+        <div className="col">
+          <input
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            className="form-control form-control-lg"
+          />
+        </div>
+      </div>
+      <br />
+    </>
+  );
+};
+
+export default AddTaskForm;
