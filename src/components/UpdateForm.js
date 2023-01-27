907bf9d029d598275d5fb89dd8d598960551f504
yeabtsega45 <yabtsega2022@gmail.com>
2023-01-27 13:07:22 +0300

    creating update form component

---
+++ src/components/UpdateForm.js
@@ -0,0 +1,16 @@
+const UpdateForm = ({ updateData, changeTask, updateTask, cancelUpdate }) => {
+  return (
+    <>
+      {/* Update Task */}
+      <div className="row">
+        <div className="col">
+          <input
+            value={updateData && updateData.title}
+            onChange={(e) => changeTask(e)}
+            className="form-control form-control-lg"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
