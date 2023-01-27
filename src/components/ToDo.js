36edc9ecf883f398a8cef4e2561fb0f3b1a1ce81
yeabtsega45 <yabtsega2022@gmail.com>
2023-01-27 14:47:13 +0300

    completed or not completed tasks

---
+++ src/components/ToDo.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const ToDo = ({ toDo, markDone, setUpdateData, deleteTask }) => {
+  return (
+    <>
+      {toDo &&
+        toDo
+          .sort((a, b) => (a.id > b.id ? 1 : -1))
+          .map((task, index) => {
+            return (
+              <React.Fragment key={task.id}>
+                <div className="col taskBg">
+                  <div className={task.status ? "done" : ""}>
+                    <span className="taskNumber">{index + 1}</span>
+                    <span className="taskText">{task.title}</span>
+                  </div>
+                </div>
+              </React.Fragment>
+            );
+          })}
+    </>
+  );
+};
+
+export default ToDo;
